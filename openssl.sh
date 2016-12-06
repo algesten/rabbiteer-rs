3c50151356bcae98227c99ab6e0bc5a86fd92d0a
Martin Algesten <martin@algesten.se>
2016-12-06 22:11:47 +0100

    remove many superflous structs. start of subscribe

--- openssl.sh
+++
@@ -1,3 +0,0 @@
-brew install openssl
-export OPENSSL_INCLUDE_DIR=`brew --prefix openssl`/include
-export OPENSSL_LIB_DIR=`brew --prefix openssl`/lib
--- src/main.rs
+++ src/main.rs
@@ -23,51 +23,6 @@ macro_rules! exitln(
     } }
 );
 
-#[derive(Debug)]
-enum Oper {
-    Publish {
-        exchange: String,
-        routing_key: String,
-        content_type: String,
-        headers: Vec<String>,
-        file: String,
-    }
-}
-
-#[derive(Debug)]
-struct Todo {
-    host: String,
-    port: u16,
-    login: String,
-    password: String,
-    vhost: String,
-    oper: Oper,
-}
-
-impl Todo {
-    fn to_opts(&self) -> amqp::Options {
-        amqp::Options {
-            host:     self.host.clone(),
-            port:     self.port.clone(),
-            login:    self.login.clone(),
-            password: self.password.clone(),
-            vhost:    self.vhost.clone(),
-            ..Default::default()
-        }
-    }
-}
-
-fn unwrap_publish(parent:&ArgMatches) -> Oper {
-    let matches = parent.subcommand_matches("publish").unwrap();
-    Oper::Publish {
-        exchange:     value_t_or_exit!(matches.value_of("exchange"), String),
-        routing_key:  value_t_or_exit!(matches.value_of("routing_key"), String),
-        content_type: matches.value_of("content_type").unwrap_or("-").to_string(),
-        headers:      values_t!(matches.values_of("header"), String).unwrap_or(vec![]),
-        file:         value_t_or_exit!(matches.value_of("file"), String),
-    }
-}
-
 fn type_from_file(file:&String) -> String {
     let t = Types::new()
         .unwrap_or_else(|e| exitln!("Error: {:?}", e));
@@ -145,42 +100,95 @@ fn main() {
                          .takes_value(true)
                     )
         )
+        .subcommand(SubCommand::with_name("subscribe")
+                    .about("Subscribe to an exchange")
+                    .arg(Arg::with_name("exchange")
+                         .help("Exchange to subscribe to")
+                         .short("e")
+                         .long("exchange")
+                         .takes_value(true)
+                         .required(true))
+                    .arg(Arg::with_name("routing_key")
+                         .help("Routing key")
+                         .short("r")
+                         .long("routing-key")
+                         .takes_value(true)
+                         .default_value(""))
+                    .arg(Arg::with_name("output")
+                         .help("Output directory (- is stdout)")
+                         .short("o")
+                         .long("output")
+                         .takes_value(true)
+                         .default_value("-")
+                    )
+        )
         .get_matches();
 
-    let todo = Todo {
-        host:     value_t_or_exit!(matches.value_of("host"), String),
-        port:     value_t_or_exit!(matches.value_of("port"), u16),
-        login:    value_t_or_exit!(matches.value_of("login"), String),
-        password: value_t_or_exit!(matches.value_of("password"), String),
-        vhost:    value_t_or_exit!(matches.value_of("vhost"), String),
-        oper:  match matches.subcommand_name() {
-            Some("publish") => unwrap_publish(&matches),
-            _ => exitln!("Error: Need subcommand. Try --help"),
-        }
-    };
-
-    let opts = todo.to_opts();
-
-    match todo.oper {
-        Oper::Publish { exchange, routing_key, content_type, headers, file } => {
-            let reader: Box<io::Read> = match file.as_ref() {
-                "-" => Box::new(io::stdin()),
-                _   => Box::new(fs::File::open(&file)
-                                .unwrap_or_else(|e| exitln!("Error: {}", e))),
-            };
-            let content_type = match content_type.as_ref() {
-                "-" => type_from_file(&file),
-                _   => content_type,
-            };
-            let sendable = client::Sendable {
-                exchange: exchange,
-                routing_key: routing_key,
-                content_type: content_type,
-                headers: headers,
-                reader: reader,
-            };
-            client::open_send(opts, sendable);
+    fn to_opts(matches:&ArgMatches) -> amqp::Options {
+        amqp::Options {
+            host:     value_t_or_exit!(matches.value_of("host"), String),
+            port:     value_t_or_exit!(matches.value_of("port"), u16),
+            login:    value_t_or_exit!(matches.value_of("login"), String),
+            password: value_t_or_exit!(matches.value_of("password"), String),
+            vhost:    value_t_or_exit!(matches.value_of("vhost"), String),
+            ..Default::default()
         }
     }
+
+    // global opts, before "publish or "subscribe"
+    let opts = to_opts(&matches);
+
+    match matches.subcommand_name() {
+
+        // execute publish comman
+        Some("publish") => {
+
+            // after the command
+            let subm = matches.subcommand_matches("publish").unwrap();
+
+            // either stdin or a file
+            let file = value_t_or_exit!(subm.value_of("file"), String);
+            let reader: Box<io::Read> = {
+                match file.as_ref() {
+                    "-" => Box::new(io::stdin()),
+                    _   => Box::new(fs::File::open(&file)
+                                    .unwrap_or_else(|e| exitln!("Error: {}", e))),
+                }
+            };
+
+            // figure out a good content type
+            // XXX should we fall back on binary octet-stream?
+            let content_type = {
+                let c = subm.value_of("content_type").unwrap_or("-").to_string();
+                match c.as_ref() {
+                    "-" => type_from_file(&file),
+                    _   => c,
+                }
+            };
+
+            // the sendable wraps up the parsed parts
+            let sendable = client::Sendable {
+                exchange: value_t_or_exit!(subm.value_of("exchange"), String),
+                routing_key: value_t_or_exit!(subm.value_of("routing_key"), String),
+                content_type: content_type,
+                headers: values_t!(subm.values_of("header"), String).unwrap_or(vec![]),
+                reader: reader,
+            };
+
+            // ship it
+            client::open_send(opts, sendable);
+        },
+
+        Some("subscribe") => {
+
+            let subm = matches.subcommand_matches("subscribe").unwrap();
+            let exchange = value_t_or_exit!(subm.value_of("exchange"), String);
+            let routing_key = value_t_or_exit!(subm.value_of("routing_key"), String);
+            let output = value_t_or_exit!(subm.value_of("output"), String);
+
+        },
+
+        _ => exitln!("Error: Need subcommand. Try --help"),
+    };
 
 }
